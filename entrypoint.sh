2aaa15cc765d5572c45a368c56b4036a3815850b
Pierre Ozoux <pierre@ozoux.net>
2016-08-24 07:27:23 +0100

    sleep 60s

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 
 while true
 do
-  START=`date +%s`
   for directory in `find /domains/ -mindepth 3 -name .git -type d -prune -not -path "/domains/trash/*" -not -path "/domains/backups/*" -not -path "*data/apps*" -not -path "*data/data*"`
   do
     cd $directory
     cd ..
     git pull
   done
-sleep $((60 - `date +%s` + $START))
+sleep 60
 done
